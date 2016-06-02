69176b52a73c5e0634d2832ed702b359595fcbf0
panavrin <sangwonlee@0587348987.wireless.umich.net>
2016-06-02 18:26:35 -0400

    canvas drawing added.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+var CanvasDrawr = function(options) {
+  var canvas = document.getElementById(options.id),
+      ctxt = canvas.getContext("2d");
+  canvas.style.width = '100%'
+  canvas.width = canvas.offsetWidth;
+  canvas.style.width = '';
+  ctxt.lineWidth = options.size || Math.ceil(Math.random() * 35);
+  ctxt.lineCap = options.lineCap || "round";
+  ctxt.pX = undefined;
+  ctxt.pY = undefined;
+  var lines = [, , ];
+  var offset = $(canvas).offset();
+  var self = {
+      init: function() {
+          canvas.addEventListener('touchstart', self.preDraw, false);
+          canvas.addEventListener('touchmove', self.draw, false);
+      },
+      preDraw: function(event) {
+          $.each(event.touches, function(i, touch) {
+              var id = touch.identifier,
+                  colors = ["red", "green", "yellow", "blue", "magenta", "orangered"],
+                  mycolor = colors[Math.floor(Math.random() * colors.length)];
+              lines[id] = {
+                  x: this.pageX - offset.left,
+                  y: this.pageY - offset.top,
+                  color: mycolor
+              };
+          });
+          event.preventDefault();
+      },
+      draw: function(event) {
+          var e = event,
+              hmm = {};
+          $.each(event.touches, function(i, touch) {
+              var id = touch.identifier,
+                  moveX = this.pageX - offset.left - lines[id].x,
+                  moveY = this.pageY - offset.top - lines[id].y;
+              var ret = self.move(id, moveX, moveY);
+              lines[id].x = ret.x;
+              lines[id].y = ret.y;
+          });
+          event.preventDefault();
+      },
+      move: function(i, changeX, changeY) {
+          ctxt.strokeStyle = lines[i].color;
+          ctxt.beginPath();
+          ctxt.moveTo(lines[i].x, lines[i].y);
+          ctxt.lineTo(lines[i].x + changeX, lines[i].y + changeY);
+          ctxt.stroke();
+          ctxt.closePath();
+          return {
+              x: lines[i].x + changeX,
+              y: lines[i].y + changeY
+          };
+      }
+  };
+  return self.init();
+};
+
 $(function() {
   var canvas = document.getElementById('multitouch_task'),
           context = canvas.getContext('2d');
@@ -14,9 +73,12 @@ $(function() {
            * Your drawings need to be inside this function otherwise they will be reset when
            * you resize the browser window and the canvas goes will be cleared.
            */
-          drawStuff();
   }
   resizeCanvas();
+  var super_awesome_multitouch_drawing_canvas_thingy = new CanvasDrawr({
+        id: "multitouch_task",
+        size: 10
+    });
 
   function drawStuff() {
           // do your drawing stuff here
