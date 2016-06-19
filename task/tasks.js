31f412158138e01ae8ac040b965ed0de3e2d7165
Sang Won Lee <sangwonlee717@gmail.com>
2016-06-18 23:54:24 -0400

    Update tasks.js

--- task/tasks.js
+++ task/tasks.js
@@ -7,6 +7,6 @@ var startingPoint2 = new Point(400,100);
 var endingPoint2 = new Point(100,400);
 var trajectory2 = new Trajectory(startingPoint2,endingPoint2 );
 var task_temp = new Task("initialTask1", [trajectory1,trajectory2]);
-var tasks = [
+tasks = [
   task_temp
 ]
