b22af7fe1e85fbae1eebe37ae2a8256802afb0a5
panavrin <sangwonlee@Sangs-MacBook-Pro.local>
2016-06-25 23:23:28 -0400

    new Audio

--- js/audio.js
+++ js/audio.js
@@ -8,6 +8,19 @@ if (!context.createDelay)
 if (!context.createScriptProcessor)
   context.createScriptProcessor = context.createJavaScriptNode;
 
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+return  window.requestAnimationFrame       ||
+  window.webkitRequestAnimationFrame ||
+  window.mozRequestAnimationFrame    ||
+  window.oRequestAnimationFrame      ||
+  window.msRequestAnimationFrame     ||
+  function( callback ){
+  window.setTimeout(callback, 1000 / 60);
+};
+})();
+
+
 function playSound(buffer, time) {
   var source = context.createBufferSource();
   source.buffer = buffer;
@@ -87,17 +100,37 @@ BufferLoader.prototype.load = function() {
   this.loadBuffer(this.urlList[i], i);
 };
 
+/*
+ * Copyright 2013 Boris Smus. All Rights Reserved.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 function OscillatorSample() {
   this.isPlaying = false;
+  this.WIDTH = 640;
+  this.HEIGHT = 240;
 }
 
 OscillatorSample.prototype.play = function() {
   // Create some sweet sweet nodes.
   this.oscillator = context.createOscillator();
+  this.analyser = context.createAnalyser();
 
   // Setup the graph.
-  this.oscillator.connect(context.destination);
+  this.oscillator.connect(this.analyser);
+  this.analyser.connect(context.destination);
 
   this.oscillator[this.oscillator.start ? 'start' : 'noteOn'](0);
 
@@ -113,9 +146,14 @@ OscillatorSample.prototype.toggle = function() {
 
 };
 
-$(function(){
+OscillatorSample.prototype.changeFrequency = function(val) {
+  this.oscillator.frequency.value = val;
+};
 
-  var sample = new OscillatorSample();
-  sample.toggle();
+OscillatorSample.prototype.changeDetune = function(val) {
+  this.oscillator.detune.value = val;
+};
 
-});
+OscillatorSample.prototype.changeType = function(type) {
+  this.oscillator.type = type;
+};
