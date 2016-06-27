9a9b2b76a08ea16a4ec8dc436de1f0b2f1db5d88
coglab-ioe <coglab.ioe@gmail.com>
2016-06-27 09:00:05 -0400

    Update tasks.js

--- task/tasks.js
+++ task/tasks.js
@@ -23,6 +23,16 @@ var trajectory4 = new Trajectory(startingPoint4,endingPoint4,100);
 var trajectory5 = new Trajectory(startingPoint5,endingPoint5,80);
 var task3 = new Task("3rdTask", [trajectory4, trajectory5]);
 
+var startingPoint6 = new Point(500,400);
+var endingPoint6 = new Point(300,400);
+var startingPoint7 = new Point(700,400);
+var endingPoint7 = new Point(900,400);
+var trajectory6 = new Trajectory(startingPoint6,endingPoint6,50);
+var trajectory7 = new Trajectory(startingPoint7,endingPoint7,50);
+var task4 = new Task("4thTask", [trajectory6, trajectory7]);
+
+
+
 tasks = [
   task1, task2, task3
 ]
