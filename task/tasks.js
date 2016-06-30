a54e4078bb4218fb30c64d89b4d0adf8bda146fd
coglab-ioe <coglab.ioe@gmail.com>
2016-06-30 11:27:32 -0400

    Update tasks.js

--- task/tasks.js
+++ task/tasks.js
@@ -1,7 +1,7 @@
 // Task 1: one-touch in 0 degree
 var startingPoint1 = new Point(500,300);
 var endingPoint1 = new Point(700,300);
-var trajectory1 = new Trajectory(startingPoint1,endingPoint1,20);
+var trajectory1 = new Trajectory(startingPoint1,endingPoint1,100);
 var task1 = new Task("1stTask", [trajectory1]);
 
 
