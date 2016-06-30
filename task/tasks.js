b54d07c79162114ae59dfbdf94452cc7b3b155e1
coglab-ioe <coglab.ioe@gmail.com>
2016-06-30 11:52:09 -0400

    Update tasks.js

--- task/tasks.js
+++ task/tasks.js
@@ -12,9 +12,39 @@ var task2 = new Task("one-touch in 45 degree", [trajectory2]);
 
 // Task 3: one-touch in 90 degree
 var startingPoint3 = new Point(500,300);
-var endingPoint3 = new Point(500,100);
+var endingPoint3 = new Point(500,300-200);
 var trajectory3 = new Trajectory(startingPoint3,endingPoint3,50);
-var task3 = new Task("2ndTask", [trajectory3]);
+var task3 = new Task("one-touch in 90 degree", [trajectory3]);
+
+// Task 4: one-touch in 135 degree
+var startingPoint4 = new Point(500,300);
+var endingPoint4 = new Point(500-200/1.414,300-200/1.414);
+var trajectory4 = new Trajectory(startingPoint4,endingPoint4,50);
+var task4 = new Task("one-touch in 135 degree", [trajectory4]);
+
+// Task 5: one-touch in 180 degree
+var startingPoint5 = new Point(500,300);
+var endingPoint5 = new Point(500-200,300);
+var trajectory5 = new Trajectory(startingPoint5,endingPoint5,50);
+var task5 = new Task("one-touch in 180 degree", [trajectory5]);
+
+// Task 6: one-touch in 225 degree
+var startingPoint6 = new Point(500,300);
+var endingPoint6 = new Point(500-200/1.414,300+200/1.414);
+var trajectory6 = new Trajectory(startingPoint6,endingPoint6,50);
+var task6 = new Task("one-touch in 225 degree", [trajectory6]);
+
+// Task 7: one-touch in 270 degree
+var startingPoint7 = new Point(500,300);
+var endingPoint7 = new Point(500,300+200);
+var trajectory7 = new Trajectory(startingPoint7,endingPoint7,50);
+var task7 = new Task("one-touch in 270 degree", [trajectory7]);
+
+// Task 8: one-touch in 315 degree
+var startingPoint8 = new Point(500,300);
+var endingPoint8 = new Point(500+200/1.414,300+200/1.414);
+var trajectory8 = new Trajectory(startingPoint8,endingPoint8,50);
+var task8 = new Task("one-touch in 315 degree", [trajectory8]);
 
 
 // Task 1
@@ -51,7 +81,7 @@ var task3 = new Task("2ndTask", [trajectory3]);
 //var task4 = new Task("4thTask", [trajectory6, trajectory7]);
 
 tasks = [
-  task1, task2, task3
+  task1, task2, task3, task4, task5, task6, task7, task8 
 ]
 
 //tasks = [
