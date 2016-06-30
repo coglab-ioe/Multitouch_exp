f6bac183c043f0c866db9419daf0a5c70b3c139f
coglab-ioe <coglab.ioe@gmail.com>
2016-06-30 11:43:41 -0400

    Update tasks.js

--- task/tasks.js
+++ task/tasks.js
@@ -2,13 +2,19 @@
 var startingPoint1 = new Point(500,300);
 var endingPoint1 = new Point(500+200,300);
 var trajectory1 = new Trajectory(startingPoint1,endingPoint1,50);
-var task1 = new Task("1stTask", [trajectory1]);
+var task1 = new Task("one-touch in 0 degree", [trajectory1]);
 
-// Task 2: one-touch in 90 degree
+// Task 2: one-touch in 45 degree
 var startingPoint2 = new Point(500,300);
-var endingPoint2 = new Point(500,100);
+var endingPoint2 = new Point(500+200/1.414,300-200/1.414);
 var trajectory2 = new Trajectory(startingPoint2,endingPoint2,50);
-var task2 = new Task("2ndTask", [trajectory2]);
+var task2 = new Task("one-touch in 45 degree", [trajectory2]);
+
+// Task 3: one-touch in 90 degree
+var startingPoint3 = new Point(500,300);
+var endingPoint3 = new Point(500,100);
+var trajectory3 = new Trajectory(startingPoint3,endingPoint3,50);
+var task3 = new Task("2ndTask", [trajectory3]);
 
 
 // Task 1
@@ -45,7 +51,7 @@ var task2 = new Task("2ndTask", [trajectory2]);
 //var task4 = new Task("4thTask", [trajectory6, trajectory7]);
 
 tasks = [
-  task1, task2
+  task1, task2, task3
 ]
 
 //tasks = [
