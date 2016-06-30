545567630d8af7a0c3843f93663a1426c7b80134
coglab-ioe <coglab.ioe@gmail.com>
2016-06-30 12:38:28 -0400

    Update tasks.js

--- task/tasks.js
+++ task/tasks.js
@@ -91,7 +91,7 @@ var trajectory3_1_1 = new Trajectory(startingPoint3_1_1,endingPoint3_1_1,50);
 var trajectory3_1_2 = new Trajectory(startingPoint3_1_2,endingPoint3_1_2,50);
 var task3_1 = new Task("two-touch (spread) in 0 degree", [trajectory3_1_1, trajectory3_1_2]);
 
-// Task 2_2: two-touch (spread) in 45 degree
+// Task 3_2: two-touch (spread) in 45 degree
 var startingPoint3_2_1 = new Point(500+30/1.414,300-30/1.414);
 var endingPoint3_2_1 = new Point(500+200/1.414,300-200/1.414);
 var startingPoint3_2_2 = new Point(500-30/1.414,300+30/1.414);
@@ -100,7 +100,7 @@ var trajectory3_2_1 = new Trajectory(startingPoint3_2_1,endingPoint3_2_1,50);
 var trajectory3_2_2 = new Trajectory(startingPoint3_2_2,endingPoint3_2_2,50);
 var task3_2 = new Task("two-touch (spread) in 45 degree", [trajectory3_2_1, trajectory3_2_2]);
 
-// Task 2_3: two-touch (spread) in 90 degree
+// Task 3_3: two-touch (spread) in 90 degree
 var startingPoint3_3_1 = new Point(500,300-30);
 var endingPoint3_3_1 = new Point(500,300-200);
 var startingPoint3_3_2 = new Point(500,300+30);
@@ -109,7 +109,7 @@ var trajectory3_3_1 = new Trajectory(startingPoint3_3_1,endingPoint3_3_1,50);
 var trajectory3_3_2 = new Trajectory(startingPoint3_3_2,endingPoint3_3_2,50);
 var task3_3 = new Task("two-touch (spread) in 90 degree", [trajectory3_3_1, trajectory3_3_2]);
 
-// Task 2_4: two-touch (spread) in 135 degree
+// Task 3_4: two-touch (spread) in 135 degree
 var startingPoint3_4_1 = new Point(500-30/1.414,300-30/1.414);
 var endingPoint3_4_1 = new Point(500-200/1.414,300-200/1.414);
 var startingPoint3_4_2 = new Point(500+20/1.414,300+20/1.414);
@@ -118,39 +118,31 @@ var trajectory3_4_1 = new Trajectory(startingPoint3_4_1,endingPoint3_4_1,50);
 var trajectory3_4_2 = new Trajectory(startingPoint3_4_2,endingPoint3_4_2,50);
 var task3_4 = new Task("two-touch (spread) in 135 degree", [trajectory3_4_1, trajectory3_4_2]);
 
+// Task 4_1: one-touch (inside) in 0 degree
+var startingPoint4_1 = new Point(500+200,300);
+var endingPoint4_1 = new Point(500+30,300);
+var trajectory4_1 = new Trajectory(startingPoint4_1,endingPoint4_1,50);
+var task4_1 = new Task("one-touch (inside) in 0 degree", [trajectory4_1]);
 
-// Task 1
-//var startingPoint1 = new Point(0,0);
-//var endingPoint1 = new Point(200,200);
-//var trajectory1 = new Trajectory(startingPoint1,endingPoint1 );
-//var startingPoint2 = new Point(400,100);
-//var endingPoint2 = new Point(100,400);
-//var trajectory2 = new Trajectory(startingPoint2,endingPoint2 );
-//var task1 = new Task("1stTask", [trajectory1,trajectory2]);
+// Task 4_2: one-touch (inside) in 45 degree
+var startingPoint4_2 = new Point(500+200/1.414,300-200/1.414);
+var endingPoint4_2 = new Point(500+30/1.414,300-30/1.414);
+var trajectory4_2 = new Trajectory(startingPoint4_2,endingPoint4_2,50);
+var task4_2 = new Task("one-touch (inside) in 45 degree", [trajectory4_2]);
 
-// Task 2
-//var startingPoint3 = new Point(300,300);
-//var endingPoint3 = new Point(300,50);
-//var trajectory3 = new Trajectory(startingPoint3,endingPoint3,20);
-//var task2 = new Task("2ndTask", [trajectory3]);
+// Task 4_3: one-touch (inside) in 90 degree
+var startingPoint4_3 = new Point(500,300-200);
+var endingPoint4_3 = new Point(500,300-30);
+var trajectory4_3 = new Trajectory(startingPoint4_3,endingPoint4_3,50);
+var task4_3 = new Task("one-touch (inside) in 90 degree", [trajectory4_3]);
 
-// Task 3
-//var startingPoint4 = new Point(100,200);
-//var endingPoint4 = new Point(120,400);
-//var startingPoint5 = new Point(200,200);
-//var endingPoint5 = new Point(220,400);
-//var trajectory4 = new Trajectory(startingPoint4,endingPoint4,100);
-//var trajectory5 = new Trajectory(startingPoint5,endingPoint5,80);
-//var task3 = new Task("3rdTask", [trajectory4, trajectory5]);
+// Task 4_4: one-touch (inside) in 135 degree
+var startingPoint4_4 = new Point(500-200/1.414,300-200/1.414);
+var endingPoint4_4_= new Point(500-30/1.414,300-30/1.414);
+var trajectory4_4 = new Trajectory(startingPoint4_4,endingPoint4_4,50);
+var task4_4 = new Task("one-touch (inside) in 135 degree", [trajectory4_4]);
 
-// Task 4
-//var startingPoint6 = new Point(500,400);
-//var endingPoint6 = new Point(300,400);
-//var startingPoint7 = new Point(700,400);
-//var endingPoint7 = new Point(900,400);
-//var trajectory6 = new Trajectory(startingPoint6,endingPoint6,50);
-//var trajectory7 = new Trajectory(startingPoint7,endingPoint7,50);
-//var task4 = new Task("4thTask", [trajectory6, trajectory7]);
+
 
 //tasks = [
 //  task1_1, task1_2, task1_3, task1_4, task1_5, task1_6, task1_7, task1_8
@@ -160,6 +152,10 @@ var task3_4 = new Task("two-touch (spread) in 135 degree", [trajectory3_4_1, tra
 //  task2_1, task2_2, task2_3, task2_4
 //]
 
+//tasks = [
+//  task3_1, task3_2, task3_3, task3_4
+//]
+
 tasks = [
-  task3_1, task3_2, task3_3, task3_4
+  task4_1, task4_2, task4_3, task4_4
 ]
